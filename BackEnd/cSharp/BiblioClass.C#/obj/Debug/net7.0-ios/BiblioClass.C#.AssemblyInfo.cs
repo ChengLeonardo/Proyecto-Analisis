1d06bfa0a7b4273c3e5667a8708784c6ee58b141
ChengLeonardo <leonardo.chenget12de1@gmail.com>
2024-10-02 15:50:06 -0300

    add gitignore

--- BackEnd/cSharp/BiblioClass.C#/obj/Debug/net7.0-ios/BiblioClass.C#.AssemblyInfo.cs
+++ BackEnd/cSharp/BiblioClass.C#/obj/Debug/net7.0-ios/BiblioClass.C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiblioClass.C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35613ecc8487641b753ea08095452b2f3dd32178")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c146868bcf65c841a38650e80a93cd16f7441d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiblioClass.C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiblioClass.C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
